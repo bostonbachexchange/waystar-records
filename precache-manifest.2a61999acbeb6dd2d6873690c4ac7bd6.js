ae35866c4a23a864fa7b50093a27ba3a9f267dae
Jacob Clapper <105309528+bostonbachexchange@users.noreply.github.com>
2024-03-12 17:02:35 -0400

    Updates

---
+++ precache-manifest.2a61999acbeb6dd2d6873690c4ac7bd6.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "553a310eb2df19c45316e3db318d651b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "772f850db1f06ed771a4",
+    "url": "/static/css/main.82766f31.chunk.css"
+  },
+  {
+    "revision": "8b7d4e0c63a74d9b2378",
+    "url": "/static/js/2.817a9991.chunk.js"
+  },
+  {
+    "revision": "56691e352562c89c8f97",
+    "url": "/static/js/3.3b37501d.chunk.js"
+  },
+  {
+    "revision": "772f850db1f06ed771a4",
+    "url": "/static/js/main.a41708af.chunk.js"
+  },
+  {
+    "revision": "69ed8855480a453056c1",
+    "url": "/static/js/runtime~main.becd148e.js"
+  },
+  {
+    "revision": "371de304dcf9a1bc966d753b7ab3e9bc",
+    "url": "/static/media/Connor.371de304.webp"
+  },
+  {
+    "revision": "a596432a160a21d322197f808f606e5c",
+    "url": "/static/media/GerriArt.a596432a.jpeg"
+  },
+  {
+    "revision": "d1ae16b5f8d0230970a59e9943ef6a15",
+    "url": "/static/media/Greg.d1ae16b5.webp"
+  },
+  {
+    "revision": "1626a87e352e5603527b50731e40e39f",
+    "url": "/static/media/GregAlbum.1626a87e.jpeg"
+  },
+  {
+    "revision": "33b00a3352c542d63903f1f4778786bd",
+    "url": "/static/media/Kendall3.33b00a33.webp"
+  },
+  {
+    "revision": "62705d3bbb4176e8bc3e7676efa6afba",
+    "url": "/static/media/KendallBurger.62705d3b.webp"
+  },
+  {
+    "revision": "662c478bb256e54150eeaaf165529007",
+    "url": "/static/media/KendallPress.662c478b.jpeg"
+  },
+  {
+    "revision": "9bfa60e687a72ec55304370e802db0f3",
+    "url": "/static/media/MusicProduction.9bfa60e6.png"
+  },
+  {
+    "revision": "6a9dbe93839ef66ed057373d9f9ea6f6",
+    "url": "/static/media/Roman.6a9dbe93.jpg"
+  },
+  {
+    "revision": "9475cf42dea9fd90b40e963717585ddc",
+    "url": "/static/media/RomanDream.9475cf42.jpeg"
+  },
+  {
+    "revision": "4dc164025cfb93c7050c9cce2b7156d7",
+    "url": "/static/media/Shiv.4dc16402.webp"
+  },
+  {
+    "revision": "81e9c44e5276907f6420b7ac4601c309",
+    "url": "/static/media/Tom.81e9c44e.jpeg"
+  },
+  {
+    "revision": "dd1ff2bba2ea2fb007455acd182b2f65",
+    "url": "/static/media/artistdev.dd1ff2bb.webp"
+  },
+  {
+    "revision": "4ae378924ee338d3f8f7ed9fc31c6e78",
+    "url": "/static/media/kendall.4ae37892.jpg"
+  },
+  {
+    "revision": "02552537d8fd04e3403a17597cfa87a8",
+    "url": "/static/media/kendall2.02552537.jpg"
+  },
+  {
+    "revision": "976e783597bdeb6d5ec269ace821ed0d",
+    "url": "/static/media/kendallaward.976e7835.jpeg"
+  },
+  {
+    "revision": "f84a4e0ef1e6319ca0eb8019eaa920c6",
+    "url": "/static/media/romanNight.f84a4e0e.webp"
+  },
+  {
+    "revision": "58bf0b2fb530eade1e13f087c0472fa4",
+    "url": "/static/media/shiv2.58bf0b2f.webp"
+  },
+  {
+    "revision": "cb269f67bfea8245dee16498a8dbd491",
+    "url": "/static/media/shivtest.cb269f67.avif"
+  },
+  {
+    "revision": "8fa155b4e38dd51c673f683d804dd3be",
+    "url": "/static/media/tomalbum.8fa155b4.jpeg"
+  }
+]);
